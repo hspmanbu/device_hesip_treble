4dabff49d9a6949ffafdb7553d8f4ecd15389a0f
hspmanbu <hesip@hesip.cn>
2017-12-21 08:46:22 +0000

    Initial commit

---
+++ treble/AndroidProducts.mk
@@ -0,0 +1,14 @@
+PRODUCT_MAKEFILES := \
+	$(LOCAL_DIR)/treble_arm64_avS.mk \
+	$(LOCAL_DIR)/treble_arm64_avN.mk \
+	$(LOCAL_DIR)/treble_arm64_agS.mk \
+	$(LOCAL_DIR)/treble_arm64_agN.mk \
+	$(LOCAL_DIR)/treble_arm64_afS.mk \
+	$(LOCAL_DIR)/treble_arm64_afN.mk \
+	$(LOCAL_DIR)/treble_arm64_bvS.mk \
+	$(LOCAL_DIR)/treble_arm64_bvN.mk \
+	$(LOCAL_DIR)/treble_arm64_bgS.mk \
+	$(LOCAL_DIR)/treble_arm64_bgN.mk \
+	$(LOCAL_DIR)/treble_arm64_bfS.mk \
+	$(LOCAL_DIR)/treble_arm64_bfN.mk \
+
---
+++ treble/treble_arm64_avN.mk
@@ -0,0 +1,10 @@
+include build/make/target/product/treble_common.mk
+$(call inherit-product, device/phh/treble/base.mk)
+
+
+PRODUCT_NAME := treble_arm64_avN
+PRODUCT_DEVICE := generic_arm64_a
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := Phh-Treble vanilla
+
+PRODUCT_PACKAGES += 
