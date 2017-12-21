9ae0588df31ad2c31bd6d20d827d5915ee602c0d
manbu_he <wudixiaozihsq@gmail.com>
2017-12-21 21:28:10 +0800

    fix

--- arm64_a_GS.mk
+++ arm64_a_GS.mk
@@ -2,7 +2,7 @@ include build/make/target/product/treble_common.mk
 $(call inherit-product, device/he/treble/base.mk)
 $(call inherit-product, device/he/treble/gapps.mk)
 
-PRODUCT_NAME := treble_arm64_agS
+PRODUCT_NAME := arm64_a_GS
 PRODUCT_DEVICE := generic_arm64_a
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := Treble with GS
