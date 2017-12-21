f6e151976b80f06f21d54d4403f4d6973c9c13a7
manbu_he <wudixiaozihsq@gmail.com>
2017-12-21 18:21:25 +0800

    Update arm64_a_GS.mk

--- arm64_a_GS.mk
+++ arm64_a_GS.mk
@@ -1,6 +1,6 @@
 include build/make/target/product/treble_common.mk
-$(call inherit-product, device/phh/treble/base.mk)
-$(call inherit-product, device/phh/treble/gapps.mk)
+$(call inherit-product, device/he/treble/base.mk)
+$(call inherit-product, device/he/treble/gapps.mk)
 
 PRODUCT_NAME := treble_arm64_agS
 PRODUCT_DEVICE := generic_arm64_a
