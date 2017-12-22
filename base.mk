24846fcac87e8128cd03a3176598e3679218b8d3
manbu_he <wudixiaozihsq@gmail.com>
2017-12-22 08:20:10 +0800

    Update base.mk

--- base.mk
+++ base.mk
@@ -8,15 +8,15 @@ PRODUCT_COPY_FILES := \
 	device/hesip/treble/copy/files/apns-conf.xml:system/etc/apns-conf.xml
 
 #Copy ES file manager
-PRODUCT_COPY_FILES := \
-	device/hesip/treble/copy/files/es.apk:system/app/es/es.apk
+#PRODUCT_COPY_FILES := \
+#	device/hesip/treble/copy/files/es.apk:system/app/es/es.apk
 
 #Copy phone-location data
 PRODUCT_COPY_FILES := \
 	device/hesip/treble/copy/files/phonelocation.dat:system/media/location/suda-phonelocation.dat
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/phh/treble/sepolicy
-DEVICE_PACKAGE_OVERLAYS += device/phh/treble/overlay
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/hesip/treble/sepolicy
+DEVICE_PACKAGE_OVERLAYS += device/hesip/treble/overlay
 
 $(call inherit-product, vendor/hardware_overlay/overlay.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
